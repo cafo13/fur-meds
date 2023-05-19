686e84a5f3030eaf68d5276bb3681187256d0b05
cafo13 <yannik.foth97@gmail.com>
2023-05-19 08:45:25 +0200

    added fastlane setup for android

--- app/capacitor.config.ts
+++ app/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.cf.furmeds',
   appName: 'fur-meds',
   webDir: 'www',
   server: {
