39357c14a6a10331bc69f3cbc46192d81a072f89
cafo13 <yannik.foth97@gmail.com>
2023-05-23 12:23:28 +0200

    reverted and removed public api key from firebaseconfig

--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -1,12 +1,7 @@
+import { firebaseConfig } from '../../firebase.config';
+
 export const environment = {
   production: false,
   apiEndpoint: 'https://pets-api-fh65cjqo3q-ez.a.run.app/api/v1',
-  firebaseConfig: {
-    apiKey: 'AIzaSyAKNDFsLex3sFeS887HKjAv68NeGmqVoxA',
-    authDomain: 'fur-meds-project.firebaseapp.com',
-    projectId: 'fur-meds-project',
-    storageBucket: 'fur-meds-project.appspot.com',
-    messagingSenderId: '136935794817',
-    appId: '1:136935794817:web:a261e9056c2150b66c8580',
-  },
+  firebaseConfig,
 };
